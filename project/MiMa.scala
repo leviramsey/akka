e0f2eff299576fbfc06052e5c6a28b51ef160aee
Sebastian Alfers <sebastian.alfers@lightbend.com>
2026-09-14 08:41:36 +0200

    chore: MiMa for 2.10.22 (#32991)

--- project/MiMa.scala
+++ project/MiMa.scala
@@ -22,7 +22,7 @@ object MiMa extends AutoPlugin {
   private val firstPatchOf29 = 0
   private val latestPatchOf29 = 8
   private val firstPatchOf210 = 0
-  private val latestPatchOf210 = 21
+  private val latestPatchOf210 = 22
 
   override def requires = MimaPlugin
   override def trigger = allRequirements
